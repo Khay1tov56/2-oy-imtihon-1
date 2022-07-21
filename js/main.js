6d3ad50ee2a53929a86104877a2f4ace151beeec
BORETS2002 <BORETS2002@example.com>
2022-07-21 18:41:40 +0300

    kontakt-page

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 var elModalOpenButton = document.querySelector(".js-modal-open-button");
+var elModalCloseButton = document.querySelector(".js-close-modal");
 var elModal = document.querySelector(".modal");
 
 elModalOpenButton.addEventListener("click", function () {
-  elModal.classList.add(".modal-show");
+  elModal.classList.add("modal-show");
 });
-
-var elModalCloseButton = document.querySelector(".js-close-modal");
 
 elModalCloseButton.addEventListener("click", function () {
-  elModal.classList.remove(".modal-show");
+  elModal.classList.remove("modal-show");
 });
+
+// const elCloseBtn = document.querySelector(".js-close-modal");
+
+// elCloseBtn.addEventListener("click", ()=>{
+//   elModal.classList.remove("modal-show");
+// })
